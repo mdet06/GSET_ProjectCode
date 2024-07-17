9a7f54d07f7dffd3db4cab5fb6e93c92ede5d75c
mdet06 <madisondetrick06@gmail.com>
2024-07-16 20:42:55 -0400

    Update Final_With_Functions.py

--- microvascularpython/Final_With_Functions.py
+++ microvascularpython/Final_With_Functions.py
@@ -139,6 +139,18 @@ def get_Centerline(input_image):
         cv2.line(centerline_img, (cols - 1, righty), (0, lefty), 255, 1)
 
 
+
+def to_micrometers(width_micrometers, height_micrometers):
+
+    dpi_per_um = 39.3701  # Conversion factor from inches to micrometers
+    width_pixels = int(width_micrometers * dpi_per_um)
+    height_pixels = int(height_micrometers * dpi_per_um)
+
+    # Resize the image
+    img = img.resize((width_pixels, height_pixels))
+    return img
+
+
 # this is incase you want to put the image into multiple filters
 a = main(img)
 b = to_Kernel(a)
