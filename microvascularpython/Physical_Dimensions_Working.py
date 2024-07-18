7a4622f2b553430a2a951fc11136a43f7528423f
mdet06 <madisondetrick06@gmail.com>
2024-07-18 14:55:59 -0400

    Update Physical_Dimensions_Working.py

--- microvascularpython/Physical_Dimensions_Working.py
+++ microvascularpython/Physical_Dimensions_Working.py
@@ -1,17 +1,30 @@
+
 from PIL import Image
 
+#make it so the new physical dimension coordinates are stored on array
 
-image_file = "/Users/madisondetrick/Documents/VisualStudios/Flower.png"
-im = Image.open(image_file)
-x = im.size[0] # return value is a tuple, ex.: (1200, 800)
+file = input("Paste file path: ")
 
-y = im.size[1]
+h  = int(input("height of the image in physical dimeninsions (um): "))
+w  = int(input("width of the image in physical dimeninsions (um): "))
+im = Image.open(file)
+
+#r, g, b = im.getpixel((x,y))
+            # if (r==255) and (g == 255) and (b == 255):
+            #     final_White_Coord.append(get_Coord_Dimen_x( w, x), get_Coord_Dimen_y(h, y))
+
+Coord = []
+
+#implement h and w variable
+def put_Coord_Dimen(dimen_w, dimen_h):
+    pixel_x = im.size[0]
+    pixel_y = im.size[1]
+    for x in range (pixel_x):
+        for y in range(pixel_y):
+            Coord.append((get_Coord_Dimen_x( dimen_w, x), get_Coord_Dimen_y(dimen_h, y)))
 
 
-
-#a is micro meter length for x, b is micro meter length for y, x is pixel length for x coord, y is pixel length for y coord
-
-
+#Coord.append(get_Coord_Dimen_x( dimen_w, x), get_Coord_Dimen_y(dimen_h, y))
 
 def get_Coord_Dimen_x( a, x):
    pixel_length_x = im.size[1]
@@ -25,9 +38,6 @@ def get_Coord_Dimen_y( b, y):
    return (y/pixel_length_y) * b
    
     
+put_Coord_Dimen(h,w)
 
-ab = get_Coord_Dimen_x( 5, 6)
-cd = get_Coord_Dimen_y( 5,6)
-
-print(ab)
-print(cd)
+print(Coord)
